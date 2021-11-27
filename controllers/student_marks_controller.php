0d40350abacef2b14b8e5597da884a19ee023fbf
Dohd <alloicebrian@gmail.com>
2021-11-27 18:12:54 +0300

    Update student_marks controller

--- controllers/student_marks_controller.php
+++ controllers/student_marks_controller.php
@@ -1,9 +1,23 @@
 <?php
 
-if ($_POST['id'] == 'student_mark') {
-    // store data
+require_once 'helpers.php';
+
+/**
+ * Form POST
+ */
+if (isset($_POST['id']) && $_POST['id'] ==  'student_mark') {
+    unset($_POST['id']);
+
     $stmt = $pdo->prepare('INSERT INTO student_marks(mark, student_id, subject_id) VALUES(?,?,?)');
     $stmt->execute([$_POST['mark'], $_POST['student_id'], $_POST['subject_id']]);
 
-    unset($_POST['id']);
+    // redirect
+    header('Location: /student_marks.php');
+    exit();    
 }
+
+/**
+ * Fetch data
+ */
+$student_subjects = $_SESSION['student_subjects'];
+$subject_rows = $_SESSION['subjects'];
