3295f59eca9400d35277de35404db1b83f01a7f0
Dohd <alloicebrian@gmail.com>
2021-11-27 18:13:52 +0300

    Update students_controller

--- controllers/students_controller.php
+++ controllers/students_controller.php
@@ -1,10 +1,17 @@
 <?php
 
-$student_rows = $pdo->query('SELECT * FROM students')->fetchAll(PDO::FETCH_ASSOC);
 
-if ($_POST['id'] == 'student') {
-    // store data
+if (isset($_POST['id']) && $_POST['id'] ==  'student') {
+    unset($_POST['id']);
     $pdo->prepare('INSERT INTO students(name) VALUES(?)')->execute([$_POST['name']]);
 
-    unset($_POST['id']);
+    // redirect
+    header('Location: /students.php');
+    exit();
 }
+
+// fetch student rows
+$student_rows = $pdo->query('SELECT * FROM students')->fetchAll(PDO::FETCH_ASSOC);
+
+// store in session
+$_SESSION['students'] = $student_rows;
