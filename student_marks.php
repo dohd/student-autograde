39dff8956246689d628b4b37ada2dba26b61af84
Dohd <alloicebrian@gmail.com>
2021-11-27 18:07:47 +0300

    Update student_marks page

--- student_marks.php
+++ student_marks.php
@@ -1,12 +1,9 @@
 <?php
 
-require_once 'header.php';
+include 'header.php';
+include 'nav-menu.php';
 
-require_once 'database.php';
-
-require_once 'controllers/students_controller.php';
-require_once 'controllers/subjects_controller.php';
-require_once 'controllers/student_marks_controller.php';
+require 'controllers/student_marks_controller.php';
 
 ?>
 
@@ -16,23 +13,18 @@ require_once 'controllers/student_marks_controller.php';
         <form class="form-inline mb-2" action="/" method="POST">
             <input type="hidden" name="id" value="student_mark">
             <div class="input-group">
-                <select name="student_id" class="form-control mr-2">
+                <select name="student_id" class="form-control mr-2" id="student-id" required>
                     <option>-- select student --</option>
-                    <?php foreach($student_rows as $row): ?>
-                        <option value="<?php echo $row['id'] ?>">
-                            <?php echo $row['name'] ?>
+                    <?php foreach($student_subjects as $student): ?>
+                        <option value="<?php echo $student['id'] ?>">
+                            <?php echo $student['name'] ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
             </div>
             <div class="input-group">
-                <select name="subject_id" class="form-control mr-2">
-                    <option>-- select subject --</option>
-                    <?php foreach($subject_rows as $row): ?>
-                        <option value="<?php echo $row['id'] ?>">
-                            <?php echo $row['name'] ?>
-                        </option>
-                    <?php endforeach; ?>
+                <select name="subject_id" class="form-control mr-2" id="subject-id" required>
+                    <option value="0">-- select subject --</option>
                 </select>
             </div>
             <div class="input-group">
@@ -58,4 +50,27 @@ require_once 'controllers/student_marks_controller.php';
         </div>
     </div>
 </div>
+
+<?php $student_subjects = json_encode($student_subjects); ?>
+
+<script>
+    const student_subjects = <?php echo $student_subjects ?>;
+    // on selecting student
+    $('#student-id').change(function() {
+        // reinitialize select
+        $('#subject-id').html($('<option/>', {text: '-- select subject --', value: 0}));
+        // assign student subjects
+        for (let i=0; i<student_subjects.length; i++) {
+            const student_id = student_subjects[i]['id'];
+            if (student_id === $(this).val()) {
+                const subjects = student_subjects[i]['subject'];
+                subjects.forEach(function(v) {
+                    $('#subject-id').append($('<option/>', {text: v.name, value: v.id}));
+                });                
+                break;
+            }
+        }
+    });
+</script>
+
 <?php include 'footer.php'; ?>
