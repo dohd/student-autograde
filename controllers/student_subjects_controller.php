703940730deb90f744385013fd054f325e59c88b
Dohd <alloicebrian@gmail.com>
2021-11-27 18:13:34 +0300

    Update student_subjects controller

--- controllers/student_subjects_controller.php
+++ controllers/student_subjects_controller.php
@@ -1,24 +1,25 @@
 <?php
 
-if ($_POST['id'] == 'student_subject') {
+require_once 'helpers.php';
+require_once 'services/subject_conditions_service.php';
+
+/**
+ * Form handler
+ */
+if (isset($_POST['id']) && $_POST['id'] ==  'student_subject') {
+    unset($_POST['id']);
     $student_id = $_POST['student_id'];
     $subject_ids = $_POST['subject_id'];
-
-    // check if subject count is between 7 and 9
-    $n = count($subject_ids);
-    if ($n < 7 || $n > 9) {
-        return error_alert('At least 7 or at most 9 subjects required!');
-    }
     
     // db query
-    $placeholder = str_repeat('?,', $n - 1) . '?';
+    $placeholder = str_repeat('?,', count($subject_ids) - 1) . '?';
     $sql = 'SELECT * FROM subjects WHERE id IN ('.$placeholder.')';
     // fetch all subjects with given ids
     $stmt = $pdo->prepare($sql);
     $stmt->execute($subject_ids);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // form array of names from results
+    // extract subject names
     $subjects = array();
     foreach($results as $result) {
         $subjects[] = $result['name'];
@@ -29,37 +30,71 @@ if ($_POST['id'] == 'student_subject') {
     $sciences = array('chemistry', 'biology', 'physics');
     $humanities = array('geography','cre','history');
 
-    if ($n == 7) {
-        // check for the compulsory 3 subjects
-        foreach($mains as $value) {
-            if (!in_array($value, $subjects)) {
-                return error_alert($value . ' is required!');
-            }
-        }
-
-        // check if science pair exists
-        if (in_array($sciences[0], $subjects) && in_array($sciences[1], $subjects)) {
-            // 
-        } elseif (in_array($sciences[0], $subjects) && in_array($sciences[2], $subjects)) {
-            // 
-        } elseif (in_array($sciences[1], $subjects) && in_array($sciences[2], $subjects)) {
-            // 
-        } else {
-            return error_alert('At least 2 sciences out of chemistry, biology and physics required!');
-        } 
-
-        // check for at least one humanity
-        foreach($humanities as $value) {
-            if (in_array($value, $subjects)) {
-                $humanity = true;
-            }
-        }
-        if (!isset($humanity)) {
-            return error_alert('At least 1 humanity out of geography, cre and history required!');
-        }
-
-        browser_log($results);
+    switch (count($subjects)) {
+        case 7:
+            $res = condition_for_seven($subjects, compact('mains','sciences','humanities'));
+            break;
+        case 8:
+            $res = condition_for_eight($subjects, compact('mains','sciences','humanities'));
+            break;
+        case 9:
+            $res = condition_for_nine($subjects, compact('mains','sciences','humanities'));
+            break;
+        default:
+            error_alert('At least 7 or at most 9 subjects required!');
     }
-    
-    unset($_POST['id']);
+
+    // save valid subjects with corresponsing student
+    if (isset($res)) {
+        foreach ($results as $result) {
+            $stmt = $pdo->prepare('INSERT INTO student_subjects(student_id, subject_id) VALUES(?,?)');
+            $stmt->execute([$student_id, $result['id']]);
+        }
+        // redirect
+        header('Location: /student_subjects.php');
+        exit();    
+    }
 }
+
+/**
+ * Fetch data
+ */
+
+ // get from session
+$student_options = $_SESSION['students'];
+$subject_options = $_SESSION['subjects'];
+
+// fetch student rows
+$sql = '
+SELECT std.id, std.name FROM students std 
+INNER JOIN student_subjects ss ON ss.student_id = std.id
+GROUP BY std.id
+';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$student_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// fetch subject rows
+$sql = '
+SELECT subj.id, subj.name, ss.student_id FROM subjects subj
+INNER JOIN student_subjects ss ON ss.subject_id = subj.id
+';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$subject_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// associate each student with corresponding subjects
+$student_subjects = array();
+foreach ($student_rows as $student_row) {
+    $student = $student_row;
+    $student['subject'] = array();
+    foreach ($subject_rows as $subject_row) {
+        if ($student_row['id'] == $subject_row['student_id']) {
+            $student['subject'][] = $subject_row;
+        }
+    }
+    $student_subjects[] = $student;
+}
+
+// store in session
+$_SESSION['student_subjects'] = $student_subjects;
