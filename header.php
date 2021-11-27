327394974d2b1b6ad50f73fe7a6647a60e978af7
Dohd <alloicebrian@gmail.com>
2021-11-27 18:05:40 +0300

    Remove nav-menu from header

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<?php require 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,30 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/css/bootstrap-multiselect.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.js"></script>
     <title>App</title>
 </head>
-<style>
-    a.nav-link {
-        color: white!important;
-    }
-    a.nav-link:hover {
-        color: rgba(255, 255, 255, 0.5)!important;
-    }
-</style>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container">
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href="students.php">Students</a></li>
-                <li class="nav-item"><a class="nav-link" href="subjects.php">Subjects</a></li>
-                <li class="nav-item"><a class="nav-link" href="student_subjects.php">Student Subjects</a></li>
-                <li class="nav-item"><a class="nav-link" href="student_marks.php">Student Marks</a></li>
-                <li class="nav-item"><a class="nav-link" href="student_ranks.php">Student Ranks</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
