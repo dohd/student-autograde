13b21ece90775d997563a8eace24a56154e26790
Dohd <alloicebrian@gmail.com>
2021-11-27 18:06:11 +0300

    Remove permute_subjects method from helpers

--- helpers.php
+++ helpers.php
@@ -19,23 +19,3 @@ function error_alert($msg='') {
         </button>
         </div>';
 }
-
-// permute without repetition with sample of 2
-function permute_subjects($list=array(), $store=array())
-{
-    $l = count($list) - 1;
-    for ($i = 0; $i < $l; $i++) {
-        // store pair (first and next) elements
-        $store[] = array($list[0], $list[$i+1]);
-        // if last index of loop
-        if ($i == $l-1) {
-            // reduce the list by the first element
-            array_shift($list);
-            // if list size is greater than one, recurse
-            if (count($list) > 1) {
-                return permute_subjects($list, $store);
-            }
-        }
-    }
-    return $store;
-}
