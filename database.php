beda1edb2e99d1529a8dba0121cec37739f2fe0d
Dohd <alloicebrian@gmail.com>
2021-11-28 09:02:00 +0300

    Add query function

--- database.php
+++ database.php
@@ -3,3 +3,18 @@
 // initialize pdo instance for database connection
 $pdo = new PDO('sqlite:app.db');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+function query($sql='', $params=[])
+{
+    global $pdo;
+
+    $stmt = $pdo->prepare($sql);
+    if ($params) {
+        $stmt->execute($params);
+    } else {
+        $stmt->execute();
+    }
+    
+    if (stripos($sql, 'SELECT') === false) return;    
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
