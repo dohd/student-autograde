87b56631dcffe9b65f2630b4c90cba43bb257a5d
Dohd <alloicebrian@gmail.com>
2022-03-26 23:45:32 +0300

    Add stream_id to students_controller

--- controllers/students_controller.php
+++ controllers/students_controller.php
@@ -4,7 +4,10 @@ if (isset($_POST['id']) && $_POST['id'] ==  'student') {
     unset($_POST['id']);
 
     try {
-        query('INSERT INTO students(reg_no,name) VALUES(?,?)', array($_POST['reg_no'], $_POST['name']));
+        query(
+            'INSERT INTO students(reg_no,name,stream_id) VALUES(?,?,?)', 
+            array($_POST['reg_no'], $_POST['name'], $_POST['stream_id'])
+        );
     } catch (\Throwable $th) {
         error_log($th->getMessage());
     }
