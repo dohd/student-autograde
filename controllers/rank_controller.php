f4ceb468c716e055362b7b2d5a522a271a73a037
Dohd <alloicebrian@gmail.com>
2022-03-27 14:25:03 +0300

    Add stream_student_scores

--- controllers/rank_controller.php
+++ controllers/rank_controller.php
@@ -10,8 +10,8 @@ function rank_formhandler($student_id, $subject_ids=[], $scores=[])
     
     try {
         $sql = 'INSERT INTO student_scores(student_id, subject_id, score) VALUES(?,?,?)';
-        foreach ($subject_ids as $key => $value) {
-            query($sql, array($student_id, $value, $scores[$key]));
+        foreach ($subject_ids as $i => $value) {
+            query($sql, array($student_id, $value, $scores[$i]));
         }
 
         // redirect
@@ -34,8 +34,9 @@ $subject_rows = query('SELECT code, name from subjects ORDER BY code');
 
 // students
 $sql = '
-SELECT std.id, std.reg_no, std.name FROM students std 
+SELECT std.id, std.reg_no, std.name, std.stream_id, str.name as stream_name FROM students std 
 INNER JOIN student_scores ss ON ss.student_id = std.id
+INNER JOIN streams as str ON str.id = std.stream_id
 GROUP BY std.id
 ';
 $students = query($sql);
@@ -52,31 +53,29 @@ $subject_scores = query($sql);
 $student_scores = array();
 foreach ($students as $student_row) {
     $student = $student_row;
+    // assign scores
     $student['scores'] = array();
     foreach ($subject_scores as $score_row) {
         if ($student_row['id'] == $score_row['student_id']) {
             $score = $score_row['score'];
-            if (empty($score)) {
-                $student['scores'][] = $score_row;
-                continue;
-            }
-            $student['scores'][] = array_merge($score_row, score_grade((int) $score));
+            if (!$score) $student['scores'][] = $score_row;
+            else $student['scores'][] = $score_row + score_grade($score);
         }
     }
+    // assign mean points and mean grade
     $points = mean_points($student['scores']);
     $student['mean_points'] = $points;
     $student['mean_grade'] = mean_grade($points);
     $student_scores[] = $student;
 }
+// assign overal positions
+$student_scores = assign_position_by_scores($student_scores);
 
-// sort student scores by mean_points and assign positions
-array_multisort(array_column($student_scores, 'mean_points'), SORT_DESC, $student_scores);
-foreach ($student_scores as $key => $value) {
-    $student_scores[$key]['position'] = $key+1;
-    if (!$key) continue;
-    $prev_points = $student_scores[$key-1]['mean_points'];
-    $prev_pos = $student_scores[$key-1]['position'];
-    if ($prev_points == $value['mean_points']) {
-        $student_scores[$key]['position'] = $prev_pos;
-    }
-}
+// group students into streams
+$stream_groups = group_by_stream($student_scores);
+// assign positions in each stream
+$stream_groups = array_reduce($stream_groups, function($init, $stream) {
+    $init[] = assign_position_by_scores($stream);
+    return $init;
+}, []);
+$stream_student_scores = array_merge(...$stream_groups);
